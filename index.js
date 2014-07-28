623d07bec046ceedc1a1739db8d0c03f8e66fcc7
jonschlinkert <dev@sellside.com>
2014-07-28 01:57:12 -0400

    layouts

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 'use strict';
 
 var _ = require('lodash');
+var Layouts = require('assemble-layouts');
+var isFalsey = require('falsey');
+var delims = require('delims');
+
+var createDelim = function (str) {
+  return str.replace(/(\s*)/g, '\\s*');
+};
 
 
 function Template(context, options) {
@@ -11,10 +18,17 @@ function Template(context, options) {
 
   this.constant('cwd', opts.cwd || process.cwd());
   this._cache('partials', opts.partials || {});
-  this._cache('layouts', opts.partials || {});
+  this._cache('layouts', opts.layouts || {});
   this._cache('tags', opts.tags || {});
 
+  this._layouts = new Layouts(opts);
   this.defaultTags();
+
+  this.layoutStack = {};
+  this.defaultLayout = '{{ body }}';
+  // this.regex = delims(layoutOpts.delims, layoutOpts).evaluate;
+  // this.regex = createDelim(this.defaultLayout);
+  this.regex = /\{{\s*body\s*}}/;
 }
 
 
@@ -94,8 +108,9 @@ Template.prototype.partials = function (key, value) {
  * Layouts
  */
 
-Template.prototype.layout = function (key, value) {
-  this.cache.layouts[key] = value;
+Template.prototype.layout = function (key, str, data) {
+  this.cache.layouts[key] = {content: str, data: data || {}};
+  return this;
 };
 
 
@@ -104,6 +119,70 @@ Template.prototype.layouts = function (key, value) {
   _.extend.apply(_, [this.cache.layouts].concat(args));
   return this;
 };
+
+
+Template.prototype.injectLayout = function (outer, inner) {
+  return outer.replace(this.regex, '\n' + inner + '\n');
+};
+
+
+Template.prototype.createStack = function (key) {
+  key = this.useLayout(key);
+  var template = null;
+  var stack = [];
+  while (key && (template = this.cache.layouts[key])) {
+    stack.unshift(key);
+    key = this.useLayout(template.data && template.data.layout);
+  }
+  return stack;
+};
+
+Template.prototype.useLayout = function (layout) {
+  if (!layout || isFalsey(layout)) {
+    return null;
+  }
+  return layout;
+};
+
+
+Template.prototype.wrap = function (str, options) {
+  var file = {
+    data: options && options.data || {},
+    content: str
+  };
+
+  var opts = _.extend({}, this.options, file.data, options);
+  var stack = this.createStack(opts.layout);
+
+  // console.log('opts', opts)
+  // console.log('stack', stack)
+
+  // Setup the object to be returned, and store file.content on `orig`
+  var results = {
+    content: this.defaultLayout
+  };
+
+  results.data = opts.data;
+  results.orig = str;
+
+  // loop over the layout stack building the context and content
+  results = stack.reduce(function (acc, key) {
+    var layout = this.cache.layouts[key];
+    acc.data = _.extend(acc.data, layout.data);
+    acc.content = this.injectLayout(acc.content, layout.content);
+    return acc;
+  }.bind(this), results);
+
+  // Pass the accumlated, final results
+  results.data = _.extend(results.data, file.data);
+  // results.content = this.injectLayout(results.content, str);
+  results.content = results.content.replace(this.regex, str);
+  if (!this.regex.test(str)) {
+    return results;
+  }
+  return results;
+};
+
 
 
 /**
@@ -128,7 +207,9 @@ Template.prototype.process = function (str, context) {
   var original = str;
 
   while (str.indexOf('${') >= 0 || str.indexOf('%>') >= 0) {
-    str = _.template(str, ctx, {imports: this.cache.tags});
+    str = _.template(this.wrap(str, ctx).content, ctx, {
+      imports: this.cache.tags
+    });
     if (str === original) {
       break;
     }
@@ -140,7 +221,7 @@ Template.prototype.process = function (str, context) {
 
 var template = new Template();
 
-console.log(template.cwd)
+// console.log(template.cwd)
 
 // template.addTag('partial', require('./tags/partial')(template.partials));
 template.addTag('include', require('./tags/include'));
@@ -154,28 +235,41 @@ template.partials({
   d: 'This is partial <%= d %>'
 });
 
+// var ctx = {
+//   a: 'A',
+//   b: 'B',
+//   c: 'C',
+//   d: 'D',
+//   layout: 'default'
+// };
+
+// console.log(template.process('<%= partial("a") %>', ctx));
+// console.log(template.process('<%= partial("b") %>', ctx));
+// console.log(template.process('<%= partial("c") %>', ctx));
+// console.log(template.process('<%= partial("d") %>', ctx));
+// console.log(template.process('<%= include("test/fixtures/a.md") %>', ctx));
+// console.log(template.process('<%= include("test/fixtures/b.md") %>', ctx));
+// console.log(template.process('<%= glob("test/fixtures/*.md") %>', ctx));
+// console.log(template.process('<%= glob(["test/fixtures/*.md"]) %>', ctx));
+
+template.constant('a', 'b');
+
+template.layout('default', 'default\n{{body}}\ndefault', {layout: 'a'});
+template.layout('a', 'BEFORE <%= a %> {{body}}AFTER <%= a %>', {layout: 'b'});
+template.layout('b', 'BEFORE <%= b %> {{body}}AFTER <%= b %>', {layout: 'c'});
+template.layout('c', 'BEFORE <%= c %> {{body}}AFTER <%= c %>');
+
+
+// var res = template.wrap('foo', {data: {layout: 'default'}});
+// console.log(res.content)
+
 var ctx = {
-  a: 'A',
-  b: 'B',
-  c: 'C',
-  d: 'D'
+  a: 'FIRST',
+  b: 'SECOND',
+  c: 'THIRD',
+  d: 'FOURTH',
+  layout: 'default'
 };
+var res = template.process('<%= partial("a") %>', ctx);
 
-console.log(template.process('<%= partial("a") %>', ctx));
-console.log(template.process('<%= partial("b") %>', ctx));
-console.log(template.process('<%= partial("c") %>', ctx));
-console.log(template.process('<%= partial("d") %>', ctx));
-console.log(template.process('<%= include("test/fixtures/a.md") %>', ctx));
-console.log(template.process('<%= include("test/fixtures/b.md") %>', ctx));
-console.log(template.process('<%= glob("test/fixtures/*.md") %>', ctx));
-console.log(template.process('<%= glob(["test/fixtures/*.md"]) %>', ctx));
-
-
-// template.constant('a', 'b');
-
-// console.log(template.a)
-
-
-
-// var str = file.readFileSync('test/fixtures/readme.md');
-// file.writeFileSync('test/fixtures/_authors.md', processTemplate(str, config, {delims: ['{%', '%}']}));
+console.log(res);
